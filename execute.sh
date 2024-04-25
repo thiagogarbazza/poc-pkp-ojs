5d09fed845a9e6895c50cd02e19836b4f666659e
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2024-04-25 09:46:36 -0300

    feat: move ojs to src folder

--- execute.sh
+++ execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu pipefail
+#set -eu pipefail
 
 COMPOSE_PROJECT_NAME="poc-pkp-ojs"
 SKIP_BUILD_APP=false
@@ -21,14 +21,17 @@ else
 fi
 
 echo "Remove old docker images and clean old executions"
-docker compose -f docker-compose.yml down --remove-orphans $CLEAN_OLD_EXECUTION
+docker compose -f .dev/docker-compose.yml down --remove-orphans $CLEAN_OLD_EXECUTION
 [[ -n "$(docker image ls -aqf=reference='poc-pkp-ojs*/*')" ]] && docker image rm -f $(docker image ls -aqf=reference="poc-pkp-ojs*/*")
 
 echo "Start ojs"
-docker compose -f docker-compose.yml up
+docker compose -f .dev/docker-compose.yml up
+
+# For enter job backup container
+# docker exec -it poc-pkp-ojs-job-backup-1 ash
 
 # For enter ojs container
-# docker exec -it poc-pkp-ojs-pkp-ojs-1 ash
+# docker exec -it poc-pkp-ojs-ojs-1 ash
 
 # For enter postgres container
 # docker exec -it poc-pkp-ojs-postgres-1 ash
