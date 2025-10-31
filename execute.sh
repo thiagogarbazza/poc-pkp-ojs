e784345ab20ab2735ef419672ff2c3c3064869da
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2025-10-31 08:23:11 -0300

    fix: update Docker and script configurations for improved functionality and consistency

--- execute.sh
+++ execute.sh
@@ -17,7 +17,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [[ "$SKIP_BUILD_APP" == "false" ]] ; then
-  bash .config/workflows/ci-build.sh
+  bash .config/workflows/ci/build.sh
   echo "Build application"
 else
   echo "Build skiped"
@@ -25,16 +25,16 @@ fi
 
 echo "Remove old docker images and clean old executions"
 docker compose --project-name poc-pkp --file .config/dev/docker-compose.yml down --remove-orphans
-[[ -n "$(docker image ls -aqf=reference='poc-pkp/*')" ]] && docker image rm -f $(docker image ls -aqf=reference="poc-pkp/*")
+[[ -n "$(docker image ls -aqf=reference='pocs/pkp*')" ]] && docker image rm -f $(docker image ls -aqf=reference="pocs/pkp*")
 
 echo "Start pkp-ojs"
 docker compose --project-name poc-pkp --file .config/dev/docker-compose.yml up --abort-on-container-failure --force-recreate --remove-orphans
 
 # For enter job backup container
-# docker exec -it poc-pkp-ojs-job-backup-1 ash
+# docker exec -it poc-pkp-job-backup-1 ash
 
 # For enter ojs container
-# docker exec -it poc-pkp-ojs-ojs-1 ash
+# docker exec -it poc-pkp-pkp-ojs-1 ash
 
 # For enter postgres container
-# docker exec -it poc-pkp-ojs-postgres-1 ash
+# docker exec -it poc-pkp-postgres-1 ash
--- src/jobs/backup/assets/run.sh
+++ src/jobs/backup/assets/run.sh
@@ -41,33 +41,36 @@ BACKUP_FILE_NAME="$(date +"%Y-%m-%d")-$TYPE"
 #### START THE BACKUPS ####
 ###########################
 
-echo "[backup] Deletando todos os diretórios de backup '$TYPE' expirados, criados a mais de $EXPIRED_DAYS dias."
+echo "[backup] Deleting all expired '$TYPE' backup created more than $EXPIRED_DAYS days ago."
 find /data/backups -maxdepth 1 -mtime +$EXPIRED_DAYS -name "*$TYPE*" \
-  -exec echo "Removendo o backup {}" \; \
+  -exec echo "Remove backup file: {}" \; \
   -exec rm -rf {} \;
 
-echo "[backup] Limpando e criando diretório temporário para a criação do backup"
+echo "[backup] Cleaning and creating temporary directory for backup creation"
 TMP_DIR=/tmp/backup-poc-pkp-ojs/$BACKUP_FILE_NAME
 rm -rf $TMP_DIR
 mkdir -p $TMP_DIR
 
-echo "[backup] Criando arquivo de backup do $OJS_FILE_DIR"
+echo "[backup] Creating backup file of $OJS_FILE_DIR"
 tar -C $OJS_FILE_DIR -czf $TMP_DIR/ojs-files.tar.gz --exclude="*.bkp" --exclude="*.log" --exclude="*.mmdb" .
 
-echo "[backup] Criando arquivo de backup do $OJS_PUBLIC_DIR"
+echo "[backup] Creating backup file of $OJS_PUBLIC_DIR"
 tar -C $OJS_PUBLIC_DIR -czf $TMP_DIR/ojs-public.tar.gz --exclude="*.bkp" --exclude="*.log" --exclude="*.mmdb" .
 
-echo "[backup] Criando arquivo de backup do banco de dados"
+echo "[backup] Creating database backup file"
 PGPASSWORD="$POSTGRES_PASSWORD" pg_dump --host="$POSTGRES_HOST" --port=5432 --username="$POSTGRES_USER" --dbname="$POSTGRES_DB" --no-owner --format="tar" > $TMP_DIR/database.bkp
 tar -C $TMP_DIR -zcf $TMP_DIR/database.tar.gz database.bkp
 rm -rf $TMP_DIR/database.bkp
 
-echo "[backup] Criando arquivo de backup geral e movendo para o diretório $BACKUP_DIR/$BACKUP_FILE_NAME.tar.gz"
+echo "[backup] Creating general backup file and moving to directory $BACKUP_DIR/$BACKUP_FILE_NAME.tar.gz"
 rm -rf $BACKUP_DIR/$BACKUP_FILE_NAME.tar.gz
 tar -C $TMP_DIR -czf $BACKUP_DIR/$BACKUP_FILE_NAME.tar.gz .
 
-echo "[backup] Limpando arquivos temporários"
+echo "[backup] Cleaning temporary files"
 rm -rf $TMP_DIR
+
+echo "[backup] All exists backup files:"
+ls -la $BACKUP_DIR
 
 if [[ "${WAIT_TO_END:-0}" -gt 0 ]]; then
   echo "Will wait for ${WAIT_TO_END} seconds before stopping..."
