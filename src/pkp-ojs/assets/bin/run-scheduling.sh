a6baf8b12738740e016f90ca46149eb7caef2123
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2026-05-09 16:49:59 -0300

    style: rename run-scheduling.sh script

--- src/pkp-ojs/assets/bin/run-scheduling.sh
+++ src/pkp-ojs/assets/bin/run-scheduling.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 echo "[pkp-ojs scheduler] running scheduler"
 php /var/www/html/lib/pkp/tools/scheduler.php run
-echo "[pkp-ojs scheduler] finished running scheduler"
+echo "[pkp-ojs scheduler] finished scheduler"
